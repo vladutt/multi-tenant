d0a82ad90b1b93bdb8d88cacdd713b826c44a679
Vladut Teodor <ilie.vladut@retargeting.biz>
2020-11-06 20:24:03 +0200

    fix users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('server');
-            $table->string('database');
             $table->rememberToken();
             $table->timestamps();
         });
